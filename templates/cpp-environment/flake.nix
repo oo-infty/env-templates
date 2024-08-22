757bcb0af8de178ee2ebba560a22c7c28dd8f3e0
oo-infty <oo-infty@outlook.com>
2024-08-22 09:15:10 +0800

    feat(cpp-environment): add aarch64-linux support

--- templates/cpp-environment/flake.nix
+++ templates/cpp-environment/flake.nix
@@ -8,13 +8,13 @@
   
   outputs = { self, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
 
       perSystem = { self', inputs', system, ... }: let
         pkgs = import inputs.nixpkgs {
           inherit system;
         };
-        mkShell = pkgs.mkShell { stdenv = pkgs.clangStdenv; };
+        mkShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; };
       in {
         devShells.default = mkShell {
           packages = with pkgs; [
